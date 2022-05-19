621c38e105651c165e598108342d8d64cf38ab2a
ibtissemdev <ibtissem.khiri@gmail.com>
2022-05-19 14:55:06 +0200

    first commit

---
+++ my-first-plugin.php
@@ -0,0 +1,10 @@
+<?php 
+// commentaires d’en-tête » avec diverses informations qui seront lues/affichées par WordPress
+
+/*
+Plugin Name: Mon premier plugin
+Description: C'est mon premier plugin 
+Author: Ibtissem
+*/
+
+?>
